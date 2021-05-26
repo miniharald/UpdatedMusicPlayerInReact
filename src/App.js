09883aeb19a0d53fa79ad6efb9c832d380bf9885
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2021-05-26 03:55:56 +0200

    added useEffect and useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 
 
 export default function App() {
   const [songs, setSongs] = useState([]);
   const [currentSong, setCurrentSong] = useState(0);
+  const [songProgress, setSongProgress] = useState(null);
+  const [progressSec, setProgressSec] = useState(0);
+  const [progressMin, setProgressMin] = useState(0);
+  const [lengthSec, setLengthSec] = useState(0);
+  const [lengthMin, setLengthMin] = useState(0);
+  const [isSongPlaying, setIsSongPlaying] = useState(false);
+  
+  const songPlaying = useRef(new Audio());
 
-  let songPlaying = new Audio();
+  useEffect(() => {
+    getSongs()
+    isSongPlaying ? songPlaying.current.pause() : songPlaying.current.play();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSongPlaying])
 
   async function getSongs() {
     setSongs(await (await fetch("data/songs.json")).json());
-    console.log(currentSong);
   }
 
-  function playSong() {
-    songPlaying.src = songs[currentSong].songLink;
-    songPlaying.play();
-  }
+  // useEffect(() => {
+  //   async function getSongs() {
+  //     if (songs.length === 0) setSongs(await (await fetch("data/songs.json")).json());
+  //   }
   
-  getSongs();
+  //   getSongs();
+  // }, []);
+
+
+  function playSong(songId) {
+    if (songPlaying.current.played) songPlaying.current.pause();
+
+    setCurrentSong(songId)
+    songPlaying.current.src = songs[songId].songLink;
+    songPlaying.current.play();
+    setIsSongPlaying(true);
+  }
+
+  function playOrPause() {
+    setIsSongPlaying(!isSongPlaying)
+    // console.log(songPlaying.current.paused);
+    // if (!isSongPlaying) {
+    //   songPlaying.current.play();
+    // } else {
+    //   console.log("hallo");
+    //   songPlaying.current.pause();
+    // }
+  }
+
+  songPlaying.current.addEventListener("timeupdate", function() {
+    setSongProgress(songPlaying.current.currentTime / songPlaying.current.duration);
+    convertTime(Math.round(songPlaying.current.currentTime));
+  });
+
+  function convertTime(seconds) {
+    setProgressMin(Math.floor(seconds / 60));
+    setProgressSec(seconds % 60);
+    showLength(Math.round(songPlaying.current.duration));
+  }
+
+  function showLength(seconds) {
+    setLengthMin(Math.floor(seconds / 60));
+    setLengthSec(seconds % 60);
+  }
 
   return (
     <div className="App">
@@ -33,21 +83,20 @@ export default function App() {
           <section id="playlist">
             {songs.map(song => {
               return (
-                <div className={song.id === currentSong ? "song active" : "song"} id={song.id} onClick={() => {
-                  setCurrentSong(song.id);
-                  playSong();
+                <div key={song.id} className={song.id === currentSong ? "song active" : "song"} id={song.id} onClick={() => {
+                  playSong(song.id);
                 }}>
                   <span>
                     <img src={"images/" + song.id + ".jpg"} alt="album"/>
                     <p><strong>{song.artist}</strong><br />{song.songTitle}</p>
                   </span>
-                  <span><i class="fas fa-play-circle"></i></span>
+                  <span><i className="fas fa-play-circle"></i></span>
                 </div>
               )
             })}
           </section>
-          <section>
-            {/* <article>
+          {songs.length > 0 ? (<section>
+            <article>
               <img id="album" src={"images/" + songs[currentSong].id + ".jpg"} alt="album" />
             </article>
             <article className="title">
@@ -61,22 +110,22 @@ export default function App() {
                 </p>
               </span>
               <span><i className="fas fa-heart"></i></span>
-            </article> */}
+            </article>
             <article className="time">
-              <span id="currenttime"><p>0:00</p></span>
-              <time><span id="progressbar"></span></time>
-              <span id="songlength"><p>0:30</p></span>
+              <span id="currenttime"><p>{progressSec < 10 ? progressMin + ":0" + progressSec : progressMin + ":" + progressSec}</p></span>
+              <time><span id="progressbar" style={{width: + songProgress * 100 + "%"}}></span></time>
+              <span id="songlength"><p>{lengthSec < 10 ? lengthMin + ":0" + lengthSec : lengthMin + ":" + lengthSec}</p></span>
             </article>
             <article className="control">
               <span id="repeat"><i className="fas fa-retweet"></i></span>
               <span id="previous"><i className="fas fa-backward"></i></span>
-              <span id="play"><i className="fas fa-play-circle"></i></span>
+              <span id="play" onClick={playOrPause}><i className={isSongPlaying ? "fas fa-pause-circle" : "fas fa-play-circle"}></i></span>
               <span id="next"><i className="fas fa-forward"></i></span>
               <span id="shuffle"
                 ><i className="fas fa-random"></i
               ></span>
             </article>
-          </section>
+          </section>) : ""}
         </main>
       </div>
     </div>
